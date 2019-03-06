b67bd6b8b17de19bac5da850a3fa1c94df995fbc
Andy Tan <andytan0727@gmail.com>
2019-03-06 23:15:12 +0800

    Initialize ui for timer app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import CircularProgressbar from "react-circular-progressbar";
+import { Button } from "semantic-ui-react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = props => {
+  const [percentage, setPercentage] = useState(0);
+
+  const handleClick = e => {
+    console.log("clicked");
+  };
+
+  useEffect(() => {
+    const circularProgress = document.querySelector(".circularProgress");
+    circularProgress.addEventListener("click", handleClick);
+
+    return () => {
+      circularProgress.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  // componentDidUpdate
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setPercentage(prevState => prevState + 1);
+    }, 250);
+    if (percentage === 100) clearTimeout(timer);
+  }, [percentage]);
+
+  return (
+    <div className="App">
+      <div className="circularProgress">
+        <CircularProgressbar percentage={percentage} text={`${percentage}%`} />
+        <Button content="Start" />
+        <input dir="rtl" id="foo" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
